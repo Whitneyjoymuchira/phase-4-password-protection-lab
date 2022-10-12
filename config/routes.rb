377ef2fadacb07d1ec994418787d7d74e5319f8d
Whitney Muchira <whitneymuchira@gmail.com>
2022-10-12 14:38:42 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+post '/signup', to: 'user#create'
+get '/me', to: 'user#show'
+
+delete 'logout', to: 'session#destroy'
+post '/login', to: 'session#create'
+get '/me', to:'session#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
